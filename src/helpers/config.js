270cc1ed1b573313b0747e892794fa1a2fe17b0d
cuibapn2014 <fakernguyen123@gmail.com>
2023-04-02 23:34:51 +0700

    Update config

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,4 +1,4 @@
 export const config = {
-    apiUrl: `${import.meta.env.VITE_API_URL}/api`,
-    baseUrl: import.meta.env.VITE_API_URL
+    apiUrl: `${import.meta.env.VITE_API_URL || process.env.VITE_API_URL}/api`,
+    baseUrl: import.meta.env.VITE_API_URL || process.env.VITE_API_URL
 }
