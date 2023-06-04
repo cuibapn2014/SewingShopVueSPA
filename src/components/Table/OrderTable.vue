91880d009aedb474eeb2e7ebc253083e2712b91b
cuibapn2014 <fakernguyen123@gmail.com>
2023-06-05 02:02:50 +0700

    Update order table component

--- src/components/Table/OrderTable.vue
+++ src/components/Table/OrderTable.vue
@@ -17,6 +17,9 @@
                 {{ index + idx }}
             </td>
             <td class="px-4 py-3 text-sm">
+                {{ item.code }}
+            </td>
+            <td class="px-4 py-3 text-sm">
                 {{ item.customer.name }}
             </td>
             <td class="px-4 py-3 text-sm">
@@ -25,14 +28,13 @@
             <td class="px-4 py-3 text-sm font-bold text-green-500">
                 {{ item.total.toLocaleString('vi') }}
             </td>
-            <td class="px-4 py-3 text-sm font-bold text-green-500">
+            <td class="px-4 py-3 text-sm">
                 <span
                     :class="{
-                        'px-2 py-1 font-semibold leading-tight rounded-full dark:text-white bg-red-100 text-red-700 dark:bg-red-600' : (item.status == 5),
+                        'px-2 py-1 font-semibold leading-tight rounded-full dark:text-white bg-red-100 text-red-700 dark:bg-red-600' : (item.status == 5 || item.status == 4),
                         'px-2 py-1 font-semibold leading-tight text-gray-700 bg-gray-100 rounded-full dark:text-gray-100 dark:bg-gray-700' : (item.status == 1),
                         'px-2 py-1 font-semibold leading-tight rounded-full dark:text-white bg-orange-100 text-orange-700 dark:bg-orange-600' : (item.status == 2),
                         'px-2 py-1 font-semibold leading-tight rounded-full dark:text-white bg-green-100 text-green-700 dark:bg-green-600' : (item.status == 3),
-                        'px-2 py-1 font-semibold leading-tight rounded-full dark:text-white bg-red-100 text-red-700 dark:bg-red-600' : (item.status == 4),
                     }">
                     {{ getStatus(item.status) }}
                 </span>
@@ -41,11 +43,14 @@
                 <img v-tooltip.top-start="item.user.name" :src="`${this.url}/img/user/${item.user.image}`" class="h-12 w-12 object-cover object-center rounded-full" loading="lazy" />
             </td>
             <td class="px-4 py-3 text-sm">
+                {{ this.$moment(item.updated_at).format('DD/MM/YYYY') }}
             </td>
             <td class="px-4 py-3 text-sm">
+                {{ this.$moment(item.NgayTraDon).format('DD/MM/YYYY') }}
             </td>
             <td class="px-4 py-3 text-sm flex items-center">
                 <button title="Chỉnh sửa" v-tooltip="'Chỉnh sửa'"
+                    @click="this.setEditId(item.id)"
                     class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
                     aria-label="Edit">
                     <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
@@ -55,6 +60,7 @@
                     </svg>
                 </button>
                 <button v-tooltip="'Xóa'" title="Xóa"
+                    @click.prevent="this.openModalDelete(item.id)"
                     class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
                     aria-label="Delete">
                     <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
@@ -63,7 +69,7 @@
                             clip-rule="evenodd"></path>
                     </svg>
                 </button>
-                <button v-tooltip="'Xuất PDF'" title="Xuất PDF"
+                <!-- <button v-tooltip="'Xuất PDF'" title="Xuất PDF"
                     class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
                     aria-label="Delete">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
@@ -71,8 +77,9 @@
                             d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v3.586l-1.293-1.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V8z"
                             clip-rule="evenodd" />
                     </svg>
-                </button>
+                </button> -->
                 <button v-tooltip="'Xem chi tiết'" title="Xem chi tiết"
+                    @click="this.openDetailModal(item.id)"
                     class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
                     aria-label="Delete">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
@@ -83,41 +90,44 @@
                     </svg>
                 </button>
 
-                <div class="inline-block relative group">
-                    <ul class="absolute hidden text-gray-700 pt-1 right-0 top-[25] group-hover:block z-50"
-                        style="margin-top: 25px;">
-                        <li class=""><a
-                                class="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
-                                href="">Duyệt</a>
-                        </li>
-                        <li class=""><a
-                                class="rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
-                                href="">Không
-                                duyệt</a></li>
-                        <li class=""><a
-                                class="rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
-                                href="">Hoàn thành</a></li>
-                        <li class=""><a
-                                class="rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
-                                href="">Hủy</a></li>
-                    </ul>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 cursor-pointer" fill="none"
-                        viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M8 12h.01M12 12h.01M16 12h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                <button 
+                v-tooltip="'Xử lý đơn hàng'"
+                @click.prevent="this.openModal(item.status, item.id)"
+                class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
+                aria-label="process-order"
+                >
+ 
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                     </svg>
-                </div>
+                </button>
             </td>
         </tr>
         <tr>
-            <td colspan="9" v-if="renderData.data?.data.length <= 0"
+            <td colspan="10" v-if="renderData.data?.data.length <= 0"
                 class="text-sm text-center py-4 text-gray-800 dark:text-gray-200">Không tìm thấy dữ liệu nào</td>
         </tr>
+        <ConfirmDeleteModal
+        :is_open="this.isOpenModal"
+        @close_delete="this.handleEventDelete"
+        @confirm_delete="this.handleEventDelete"
+        />
     </BaseTable>
+    <UpdateStatusModal :display="this.isShowModal" :status="this.status" :id="this.idUpdate" @close="closeModal" @update_success="updateStatusSuccess"/>
+    <OrderDetailModal 
+    :display="this.isOpenDetailModal" 
+    :id_order="this.idDetail"
+    @toggle_detail="() => this.isOpenDetailModal = false"/>
 </template>
 <script>
 import { config } from '../../helpers/config';
 import BaseTable from './BaseTable.vue';
+import UpdateStatusModal from '../Modal/UpdateStatusModal.vue';
+import ConfirmDeleteModal from '../Modal/ConfirmDeleteModal.vue';
+import { orderService } from '../../services/order.service';
+import { toast } from "vue3-toastify";
+import OrderDetailModal from '../Modal/OrderDetailModal.vue';
 
 export default {
     props: {
@@ -125,8 +135,11 @@ export default {
         is_load: Boolean
     },
     components: {
-        BaseTable
-    },
+    BaseTable,
+    UpdateStatusModal,
+    ConfirmDeleteModal,
+    OrderDetailModal
+},
     computed: {
         renderData() {
             let data = this.data_list ?? []
@@ -144,10 +157,17 @@ export default {
     data() {
         return {
             fieldList: [
-                'Khách hàng', 'Liên hệ', 'Tổng tiền', 'Trạng thái', 'Tạo bởi', 'Cập nhật', 'Ngày giao hàng'
+                'Mã đơn hàng', 'Khách hàng', 'Liên hệ', 'Tổng tiền', 'Trạng thái', 'Tạo bởi', 'Cập nhật', 'Ngày giao hàng'
             ],
             index: this.data_list?.data.from || 1,
-            url: config.apiUrl.split('/api')[0]
+            url: config.apiUrl.split('/api')[0],
+            isShowModal: false,
+            status: null,
+            idUpdate: null,
+            isOpenModal: false,
+            deleteId: null,
+            isOpenDetailModal: false,
+            idDetail: null
         }
     },
     methods: {
@@ -164,7 +184,55 @@ export default {
                 case 4: return "Không duyệt";
                 case 5: return "Đã hủy";
             }
-        }
+        },
+        setEditId(id) {
+            this.$emit("edit_order", id);
+        },
+        openModal(status, id) {
+            this.status = status
+            this.idUpdate = id
+            this.isShowModal = true;
+        },
+        closeModal() {
+            this.isShowModal = false;
+        },
+        openModalDelete(id) {
+            this.isOpenModal = true;
+            this.deleteId = id;
+        },
+        openDetailModal(id) {
+            this.isOpenDetailModal = true;
+            this.idDetail = id;
+        },
+        updateStatusSuccess(){
+            this.$emit('update_success_status')
+            this.isShowModal = false
+        },
+        async handleEventDelete(data) {
+            if (data) {
+                await orderService
+                .deleteById(this.deleteId)
+                .then((res) => {
+                    if (res.data.msg) {
+                    toast.success(`Xóa thành công!`, {
+                        position: toast.POSITION.TOP_RIGHT,
+                        theme: toast.THEME.COLORED,
+                        pauseOnHover: false,
+                    });
+                    this.$emit("delete_order");
+                    }
+                })
+                .catch((err) => {
+                    toast.error(`Đã xảy ra lỗi! Vui lòng kiểm tra lại`, {
+                    position: toast.POSITION.TOP_RIGHT,
+                    theme: toast.THEME.COLORED,
+                    pauseOnHover: false,
+                    });
+                });
+            }
+            this.isOpenModal = false;
+            this.deleteId = null;
+        },
     }
 }
 </script>
--- src/views/admin/Order.vue
+++ src/views/admin/Order.vue
@@ -3,7 +3,102 @@
     <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
       Đơn hàng
     </h2>
-    <OrderTable :data_list="this.dataList" :is_load="getIsLoad"/>
+    <div class="flex justify-end py-2">
+      <form method="GET" @submit.prevent="this.searchSubmit()" class="flex mx-2">
+        <input
+          class="block w-64 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray rounded-l-md form-input"
+          type="text"
+          name="keyword"
+          placeholder="Nhập tìm kiếm.."
+          v-model="this.search"
+        />
+        <button
+          class="flex items-center px-2 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border-0 rounded-r-md active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </button>
+      </form>
+      <button
+      @click="this.setEditId(null)"
+        class="flex items-center px-2 py-2 mr-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border-0 rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
+      >
+        Thêm mới
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M17 14v6m-3-3h6M6 10h2a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2zm10 0h2a2 2 0 002-2V6a2 2 0 00-2-2h-2a2 2 0 00-2 2v2a2 2 0 002 2zM6 20h2a2 2 0 002-2v-2a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2z"
+          />
+        </svg>
+      </button>
+      <button
+        class="flex items-center px-2 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-green-600 border-0 rounded-lg active:bg-green-700 hover:bg-green-700 focus:outline-none focus:shadow-outline-purple"
+      >
+        Xuất Excel
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
+          />
+        </svg>
+      </button>
+      <button
+        class="flex items-center px-2 py-2 mx-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border-0 rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
+        @click="this.reload()"
+      >
+        Tải lại
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6 ml-1"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+          />
+        </svg>
+      </button>
+    </div>
+    <OrderTable :data_list="this.dataList" :is_load="getIsLoad"
+     @edit_order="this.setEditId"
+     @delete_order="this.reFetchData()"
+     @update_success_status="this.fetchDataOrder()"/>
+    <ActionOrderModal
+      :is_open_modal="this.isOpenModal"
+      @togglemodal="this.closeModal"
+      @success_create="this.reFetchData()"
+      :id_order="this.editId"
+    />
   </div>
 </template>
 <script>
@@ -11,11 +106,12 @@ import BaseLayout from "../../components/layouts/BaseLayout.vue"
 import OrderTable from "../../components/Table/OrderTable.vue";
 import { orderService } from "../../services/order.service"
 import emitter from 'tiny-emitter/instance'
+import ActionOrderModal from "../../components/Modal/ActionOrderModal.vue";
 
 export default {
   extends: BaseLayout,
   components: {
-    BaseLayout, OrderTable
+    BaseLayout, OrderTable, ActionOrderModal
   },
   created() {
     this.fetchDataOrder()
@@ -36,7 +132,10 @@ export default {
   data() {
     return {
       dataList: null,
-      isLoadData: false
+      isLoadData: false,
+      isOpenModal: false,
+      editId: null,
+      search: null,
     }
   },
   methods: {
@@ -44,13 +143,39 @@ export default {
       this.isLoadData = true
       const urlSearch = await new URLSearchParams(window.location.search)
       let page = await urlSearch.has('page') ? (pageTo ?? urlSearch.get('page')) : (pageTo ?? 1)
-      await orderService.getDataAll(page).then(res => {
+      const data = {
+        keyword: this.search
+      };
+      await orderService.getDataAll(page, data).then(res => {
         if (res.data.code == 200)
           this.dataList = res.data
       }).then(() => {
         this.isLoadData = false
       })
-    }
+    },
+    async reFetchData() {
+      await this.fetchDataOrder();
+      await this.closeModal();
+    },
+    closeModal(data) {
+      this.isOpenModal = data;
+    },
+    openModal() {
+      this.isOpenModal = true;
+    },
+    setEditId(id) {
+      this.editId = id;
+      this.openModal();
+    },
+    searchSubmit() {
+      if (this.search && this.search != "") {
+        this.fetchDataOrder();
+      }
+    },
+    reload() {
+      this.search = null;
+      this.fetchDataOrder();
+    },
   }
 }
 </script>
