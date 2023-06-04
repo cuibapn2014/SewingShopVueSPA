2235577353b87772ee7153b6a8828be6b6939add
cuibapn2014 <fakernguyen123@gmail.com>
2023-06-05 02:04:12 +0700

    Update order service

--- src/services/order.service.js
+++ src/services/order.service.js
@@ -6,16 +6,78 @@ axios.defaults.withCredentials = true
 
 
 export const orderService = {
-    getDataAll
+    getDataAll,
+    findById,
+    updateStatus,
+    create,
+    update,
+    deleteById
 }
 
-async function getDataAll(page = null){
+async function getDataAll(page = null, data){
     const requestOptions = {
         headers: {
             ...authHeader(),
             'Content-Type': 'application/json'
         }
     };
-    let queryPage = page != null ? '?page=' + page : ''
-    return axios.get(`${config.apiUrl}/order${queryPage}`, null, requestOptions);
+    return axios.get(`${config.apiUrl}/order`, {
+        params:{
+            ...data,
+            page: page
+        }
+    },requestOptions);
+}
+
+async function findById(id){
+    const requestOptions = {
+        headers: {
+            ...authHeader(),
+            'Content-Type': 'application/json'
+        }
+    };
+
+    return axios.get(`${config.apiUrl}/order/${id}`, null, requestOptions);
+}
+
+async function create(order){
+    const requestOptions = {
+        headers: {
+            ...authHeader(),
+            'Content-Type': 'multipart/form-data'
+        }
+    };
+    return axios.post(`${config.apiUrl}/order/create`, order, requestOptions);
+}
+
+async function updateStatus(status, id){
+    const requestOptions = {
+        headers: {
+            ...authHeader(),
+            'Content-Type': 'multipart/form-data'
+        }
+    };
+    return axios.post(`${config.apiUrl}/order/update-status/${id}`, status, requestOptions);
+}
+
+async function update(order){
+    let id = order.get('id')
+    const requestOptions = {
+        headers: {
+            ...authHeader(),
+            'Content-Type': 'multipart/form-data'
+        }
+    };
+    return axios.post(`${config.apiUrl}/order/update/${id}`, order, requestOptions);
+}
+
+async function deleteById(id){
+    const requestOptions = {
+        headers: {
+            ...authHeader(),
+            'Content-Type': 'application/json'
+        }
+    };
+
+    return await axios.delete(`${config.apiUrl}/order/delete/${id}`, null, requestOptions);
 }
